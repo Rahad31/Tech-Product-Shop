b8c106e7eda3697edf294b33b311157af43a8c40
Rahad31 <rahad3100@gmail.com>
2023-10-18 20:22:44 +0600

    login & registration

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useContext } from "react";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 
@@ -7,117 +6,117 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
-    const { createUser } = useContext(AuthContext);
-    const navigate = useNavigate();
-    const handleRegister = (event) => {
-      event.preventDefault();
-      const name = event.target.name.value;
-      const email = event.target.email.value;
-      const password = event.target.password.value;
-      const image = event.target.image.value;
-      event.target.reset();
-      // console.log(image);
+  const { createUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const image = event.target.image.value;
+    event.target.reset();
+    // console.log(image);
 
-      // validation
+    // validation
 
-      if (password.length < 6) {
-        toast("Password must be at least 6 characters or longer");
-        return;
-      } else if (
-        !/^(?=.*[-\#\$\.\%\&\@\!\+\=\<\>\*])(?=.*[a-zA-Z])(?=.*\d).{8,12}$/.test(
-          password
-        )
-      ) {
-        toast(
-          "Your password should have at least one upper case characters and special character"
-        );
-        return;
-      } else {
-        createUser(email, password, name, image)
-          .then((result) => {
-            console.log(result.user);
-            toast("Successfully Registered and Login");
-            navigate("/");
-          })
-          .catch(() => {});
-      }
-    };
+    if (password.length < 6) {
+      toast("Password must be at least 6 characters or longer");
+      return;
+    } else if (
+      !/^(?=.*[-\#\$\.\%\&\@\!\+\=\<\>\*])(?=.*[a-zA-Z])(?=.*\d).{8,12}$/.test(
+        password
+      )
+    ) {
+      toast(
+        "Your password should have at least one upper case characters and special character"
+      );
+      return;
+    } else {
+      createUser(email, password, name, image)
+        .then((result) => {
+          console.log(result.user);
+          toast("Successfully Registered and Login");
+          navigate("/");
+        })
+        .catch(() => {});
+    }
+  };
 
-    return (
-      <div className="hero min-h-screen bg-[#e2e8f0] ">
-        <div className="hero-content flex-col">
-          <div className="text-center ">
-            <h1 className="text-4xl text-[#eaa334] font-bold">Register </h1>
-          </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="card-body ">
-              <form onSubmit={handleRegister}>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Name</span>
-                  </label>
-                  <input
-                    type="text"
-                    name="name"
-                    placeholder="Your Name"
-                    required
-                    className="input input-bordered"
-                  />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    required
-                    className="input input-bordered"
-                  />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Password</span>
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    required
-                    placeholder="Password"
-                    className="input input-bordered"
-                  />
-                </div>{" "}
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Upload Image URL</span>
-                  </label>
-                  <input
-                    type="text"
-                    name="image"
-                    placeholder="Image"
-                    className="input input-bordered "
-                  />
-                </div>
-                {/* <Link to="/login"> */}
-                <div className="form-control mt-6">
-                  <button className="btn btn- bg-[#eaa334]">Register</button>
-                </div>
-                {/* </Link> */}
-              </form>
-              <p className="text-center">
-                {" "}
-                Already have account?
-                <Link to="/login">
-                  <button className="btn btn-link">Login</button>
-                </Link>{" "}
-              </p>
-            </div>
+  return (
+    <div className="hero min-h-screen bg-[#e2e8f0] ">
+      <div className="hero-content flex-col">
+        <div className="text-center ">
+          <h1 className="text-4xl text-[#eaa334] font-bold">Register </h1>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body ">
+            <form onSubmit={handleRegister}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Your Name"
+                  required
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  required
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  required
+                  placeholder="Password"
+                  className="input input-bordered"
+                />
+              </div>{" "}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Upload Image URL</span>
+                </label>
+                <input
+                  type="text"
+                  name="image"
+                  placeholder="Image"
+                  className="input input-bordered "
+                />
+              </div>
+              {/* <Link to="/login"> */}
+              <div className="form-control mt-6">
+                <button className="btn btn- bg-[#eaa334]">Register</button>
+              </div>
+              {/* </Link> */}
+            </form>
+            <p className="text-center">
+              {" "}
+              Already have account?
+              <Link to="/login">
+                <button className="btn btn-link">Login</button>
+              </Link>{" "}
+            </p>
           </div>
         </div>
-        <ToastContainer />
       </div>
-    );
+      <ToastContainer />
+    </div>
+  );
 };
 
 export default Register;
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Root from "./components/Root/Root";
 import Addpro from "./components/AddPro/Addpro";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import AuthProvider from "./components/Provider/Provider";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -16,7 +19,15 @@ const router = createBrowserRouter([
       },
       {
         path: "/addproduct",
-        element:<Addpro></Addpro>
+        element: <Addpro></Addpro>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
       },
     ],
   },
@@ -24,6 +35,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      {" "}
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
